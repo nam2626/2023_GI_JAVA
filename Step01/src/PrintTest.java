11d895aace49aa2d0d3e52f1790d8be9c4706b04
nam2626 <nam2626@gmail.com>
2023-10-05 16:32:34 +0900

    단축키

--- Step01/src/PrintTest.java
+++ Step01/src/PrintTest.java
@@ -1,10 +1,22 @@
 
 public class PrintTest {
-
+	/* -> 여러줄 주석을 달아야할때
+	 * 
+	 * 주석은 코드에 설명문을 작성하는 영역
+	 * 실행이 안되는 코드
+	 */
+	//한줄 단위 주석
 	public static void main(String[] args) {
-		System.out.println("남상규");
-		System.out.print("남상규");
-		System.out.println("남상규");
+		System.out.println("남상규1");
+		System.out.print("남상규2\n"); // \n은 줄바꿈
+		System.out.println("남상규3");
+		
+		System.out.println("Hello World - 1");
+		System.out.println("Hello World - 2");
+//		System.out.println("Hello World - 3");
+		System.out.println("Hello World - 4");
+		System.out.println("Hello World - 5");
+		
 	}
 
 }
