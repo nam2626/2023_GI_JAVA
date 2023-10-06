351252104e857f5040da6b3dbb7da80a45f06289
nam2626 <nam2626@gmail.com>
2023-10-06 10:19:29 +0900

    변수

---
+++ Step02/src/E01_VariableType.java
@@ -0,0 +1,23 @@
+
+public class E01_VariableType {
+	/*
+	 * 		변수 타입
+	 * 			정수형 : byte(1) short(2) int(4) long(8)
+	 * 			실수형 : float(4) double(8)
+	 * 			문자형 : char(2)
+	 * 			논리형 : boolean(1)
+	 */
+	public static void main(String[] args) {
+		long l = 4294967296L;
+		System.out.println(l);
+		
+		short s = 100;
+		System.out.println(s);
+		
+		byte b = 127;
+		System.out.println(b);
+		
+		
+	}
+
+}
