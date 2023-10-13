62f942f4439a9c0d53725800c516405d951a51c4
nam2626 <nam2626@gmail.com>
2023-10-13 11:18:40 +0900

    배열

---
+++ Step06/src/E04_Quest1.java
@@ -0,0 +1,47 @@
+/*
+	 * 	정수형 배열 길이가 7인 배열을 생성한 뒤에
+	 * 	배열에 숫자를 전부 사용자로부터 입력을 받아서 저장한 후에
+	 * 	배열에 있는 모든 값을 출력
+	 * 
+	 * 	예시>
+	 * 	0번째 숫자 입력 : 3
+	 * 	1번째 숫자 입력 : 4
+	 *  ...
+	 *  6번째 숫자 입력 : 5
+	 * 	
+	 * 	3 4 ... 5
+	 * 
+	 * 	Arrays.toString은 사용하지 않는다.
+	 */
+
+import java.util.Scanner;
+
+public class E04_Quest1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] arr = new int[7];
+		
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(i + "번재 입력 > ");
+			arr[i] = sc.nextInt();
+		}
+		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
