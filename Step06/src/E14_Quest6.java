3ae01a20720a8c83abaef8258a9f1d8133754548
nam2626 <nam2626@gmail.com>
2023-10-13 17:37:01 +0900

    배열

--- Step06/src/E14_Quest6.java
+++ Step06/src/E14_Quest6.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class E14_Quest6 {
 	public static void main(String[] args) {
@@ -10,5 +11,31 @@ public class E14_Quest6 {
 		}
 		//배열의 내용을 출력 Arrays.toString()을 사용
 		System.out.println(Arrays.toString(arr));
+		
+		//삭제할 숫자 입력
+		Scanner sc = new Scanner(System.in);
+		System.out.print("삭제할 숫자 입력 > ");
+		int n = sc.nextInt();
+		
+		//반복문
+		for(int i=0;i<arr.length;i++) {
+			//삭제할 숫자가 있는 배열 인덱스를 찾음
+			if(arr[i] == n) {
+				//배열의 내용을 한칸씩 땡겨오는 작업
+				for(int j=i;j<arr.length-1;j++) {
+					arr[j] = arr[j+1];
+				}
+				break;
+			}
+		}
+		
+		//배열 내용을 전부 출력
+		System.out.println(Arrays.toString(arr));
 	}
 }
+
+
+
+
+
+
