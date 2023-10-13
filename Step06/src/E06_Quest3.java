a417a0d26b7fae1343a17fbc81225f14aac066ed
nam2626 <nam2626@gmail.com>
2023-10-13 12:47:22 +0900

    배열

---
+++ Step06/src/E06_Quest3.java
@@ -0,0 +1,11 @@
+
+public class E06_Quest3 {
+	/*
+	 * 	정수형 배열 길이가 10인 배열을 생성한 후에
+	 * 	정수를 입력받아서 배열의 각 요소에 전부 입력한 후
+	 * 	배열에 저장된 값들 중 최대값, 최소값, 총합을 출력
+	 */
+	public static void main(String[] args) {
+		
+	}
+}
