efd566c331e492c3ab850712f4685b302dfe5b1c
nam2626 <nam2626@gmail.com>
2023-10-18 16:46:14 +0900

    클래스

---
+++ Step07/src/ex/TV.java
@@ -0,0 +1,20 @@
+package ex;
+/*
+ * 	TV 클래스
+ * 		필드
+ * 			전원, 채널, 음량, 음소거
+ * 		기능
+ * 			전원OnOff, 채널Up/Down, 음량Up/Down, 음소거OnOff
+ * 
+ * 		전원 - boolean
+ * 		채널 - int : 1~356
+ * 		음량 - int : 0 ~ 40
+ * 		음소거 - boolean
+ * 
+ * 		기본 생성자만 생성 - 초기값은 알맞게 임의값으로 초기화
+ * 		TV 테스트 하는 클래스 TVMain도 생성해서 기능을 만들때마다 테스트하면서 구현
+ * 
+ */
+public class TV {
+
+}
