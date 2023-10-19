304da33120149eb601a84724284762f1262e56cc
nam2626 <nam2626@gmail.com>
2023-10-19 10:15:38 +0900

    문자열

--- Step07/src/string/StringEx.java
+++ Step07/src/string/StringEx.java
@@ -31,7 +31,26 @@ public class StringEx {
 		System.out.println(str2.toUpperCase());
 		//문자열 길이 리턴
 		System.out.println(str2.length());
+		//문자열 부분 추출, 10번 인덱스부터 마지막 인덱스까지 문자열을 잘라내서 리턴
+		System.out.println(str2.substring(10));
+		//문자열 부분 추출, 10번 인덱스부터 15 인덱스전까지 문자열을 잘라내서 리턴
+		System.out.println(str2.substring(10,15));
+		//문자열 바꾸기
+		System.out.println(str2.replace("World", "Hell"));
 		
+		String str3 = "          Hello World          ";
+		System.out.println(str3.length());
+		System.out.println(str3);
+		//문자열 양쪽에 필요없는 공백을 제거
+		System.out.println(str3.trim());
+		System.out.println(str3.trim().length());
+		
+		String str4 = "java,html,css,javascript,jsp,spring";
+		//문자열을 구분자를 기준으로 잘라서 배열로 만들어줌
+		String[] result = str4.split(",");
+		System.out.println(Arrays.toString(result));
+		//문자열 배열을 하나의 문자열로 구분자를 넣어서 만들어줌
+		System.out.println(String.join(",", result));
 		
 	}
 
