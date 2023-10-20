094af4051fb4ef594664cba5a3a608437c58c2dc
nam2626 <nam2626@gmail.com>
2023-10-20 10:27:53 +0900

    static

--- Step08/src/static_ex/Count.java
+++ Step08/src/static_ex/Count.java
@@ -1,5 +1,26 @@
 package static_ex;
 
 public class Count {
-
+	private int count;
+	private static int total_count = 0;
+	
+	public Count() {
+		count++;
+		total_count++;
+		System.out.println(count + " " + total_count);
+	}
+	
+	public void addStaticCount() {
+		//non-static에서는 static에 언제든지 접근이 가능
+		//non-static이 실행되는 시점에는 static이 이미 메모리에 생성되어있다.
+		total_count++;
+	}
+	
+	
+	
 }
+
+
+
+
+
