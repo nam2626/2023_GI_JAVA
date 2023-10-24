13a8570864e9009d794c7e78bd8ceec35d06481d
nam2626 <nam2626@gmail.com>
2023-10-24 12:50:12 +0900

    has a

--- Step09/src/has_a_3/PowerAppMain.java
+++ Step09/src/has_a_3/PowerAppMain.java
@@ -4,16 +4,46 @@ import java.util.Scanner;
 
 public class PowerAppMain {
 	/*
-	 * 1. 기기등록
-	 * 2. 전체 전원 On
-	 * 3. 전체 전원 Off
-	 * 0. 프로그램 종료
-	 * 원하시는 메뉴 번호 입력 : 
+	 * 기기 등록을 시작합니다.........
+	 * 1. TV
+	 * 2. Aircon
+	 * 3. Light
+	 * 등록할 기기번호 입력 : 1
+	 * TV 기기 등록완료
 	 */
+	public static PowerOnOff createDevice(Scanner sc) {
+		System.out.println("기기등록을 시작합니다...........");
+		System.out.println("1. TV");
+		System.out.println("2. Aircon");
+		System.out.println("3. Light");
+		System.out.print("등록할 기기번호 입력 : ");
+		int no = sc.nextInt();
+		
+		PowerOnOff power = null;
+		switch(no) {
+		case 1:
+			power = new TV();
+			break;
+		case 2:
+			power = new Aircon();
+			break;
+		case 3:
+			power = new Light();
+			break;
+		}
+		return power;
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		IotApp app = new IotApp();
-		
+		/*
+		 * 1. 기기등록
+		 * 2. 전체 전원 On
+		 * 3. 전체 전원 Off
+		 * 0. 프로그램 종료
+		 * 원하시는 메뉴 번호 입력 : 
+		 */
 		int no = 0;
 		do {
 			//메뉴 출력
@@ -27,10 +57,13 @@ public class PowerAppMain {
 			
 			switch(no) {
 			case 1:
+				
 				break;
 			case 2:
+				app.deviceAllPowerOn();
 				break;
 			case 3:
+				app.deviceAllPowerOff();
 				break;
 			}
 		}while(no != 0);
