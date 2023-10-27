31945f7a719cb84dceb0e19e5c3fc88bf8471a2a
nam2626 <nam2626@gmail.com>
2023-10-27 12:20:20 +0900

    Exception

---
+++ Step11/src/generic_extends/MouseHUB.java
@@ -0,0 +1,17 @@
+package generic_extends;
+
+public class MouseHUB<T extends Mouse> {
+	private T mouse;
+	
+	public MouseHUB(T mouse) {
+		this.mouse = mouse;
+	}
+
+	public void click(int n) {
+		mouse.click(n);
+	}
+	
+	public void wheel() {
+		mouse.wheel();
+	}
+}
