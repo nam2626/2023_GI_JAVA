a927b194b3ab51556eb08c7fae67bf3e097fdcac
nam2626 <nam2626@gmail.com>
2023-10-30 11:27:57 +0900

    스트림

---
+++ Stream/src/io/FileWriteMain.java
@@ -0,0 +1,39 @@
+package io;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FileWriteMain {
+
+	public static void main(String[] args) {
+		FileWriter fw = null;
+		PrintWriter pw = null;
+		try {
+			//1. 노드 스트림 초기화
+			fw = new FileWriter("hello.txt");
+			//2. 프로세스 스트림 초기화
+			pw = new PrintWriter(fw);
+			//3. 출력
+			pw.println("Hello World");
+			pw.println("안녕하세요");
+			pw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				//4. close
+				if(pw != null)pw.close();
+				if(fw != null)fw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
+
+
+
+
+
--- Stream/src/test/TestMain.java
+++ Stream/src/test/TestMain.java
@@ -6,7 +6,7 @@ public class TestMain {
 		System.out.println((int)'\r' + " " + (int)'\n');
 		IOTest io = new IOTest();
 		System.out.println(io.readline());
-//		io.println("안녕하세요");
+		io.println("안녕하세요");
 	}
 
 }
