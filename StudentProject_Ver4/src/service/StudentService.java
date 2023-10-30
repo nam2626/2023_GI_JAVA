cbd07e9c96580f2a1438d00fa03d6918630b2a2b
nam2626 <nam2626@gmail.com>
2023-10-30 15:34:08 +0900

    스트림

--- StudentProject_Ver4/src/service/StudentService.java
+++ StudentProject_Ver4/src/service/StudentService.java
@@ -1,10 +1,14 @@
 package service;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import vo.StudentVO;
 
@@ -70,62 +74,70 @@ public class StudentService {
 
 	public ArrayList<StudentVO> searchNameStudent(String name) {
 		ArrayList<StudentVO> result = new ArrayList<StudentVO>();
-		
-		for(int i=0;i<list.size();i++) {
-			if(list.get(i).getStudentName().indexOf(name) != -1)
+
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getStudentName().indexOf(name) != -1)
 				result.add(list.get(i));
 		}
-		
+
 		return result;
 	}
 
 	public ArrayList<String> getMajorList() {
-		
+
 		ArrayList<String> result = new ArrayList<String>();
-		
-		for(int i=0;i<list.size();i++) {
-			//1. 학생 정보를 하나씩 뽑음
-			//2. 학과 정보를 뽑아서 리스트에 저장(중복이 안되게끔 처리)
-			if(!result.contains(list.get(i).getMajor()))
+
+		for (int i = 0; i < list.size(); i++) {
+			// 1. 학생 정보를 하나씩 뽑음
+			// 2. 학과 정보를 뽑아서 리스트에 저장(중복이 안되게끔 처리)
+			if (!result.contains(list.get(i).getMajor()))
 				result.add(list.get(i).getMajor());
 		}
-		//3. 결과를 리턴
-		
+		// 3. 결과를 리턴
+
 		return result;
 	}
-	
-	//프로그램이 종료 직전 실행되는 메서드
-	//리스트에 있는 모든 데이터를 json으로 변경 후에
-	//student.json으로 저장하게끔 처리
+
+	// 프로그램이 종료 직전 실행되는 메서드
+	// 리스트에 있는 모든 데이터를 json으로 변경 후에
+	// student.json으로 저장하게끔 처리
 	public void saveStudentJSONFile() {
-		//1. 모든 데이터를 JSON으로 변환
+		// 1. 모든 데이터를 JSON으로 변환
 		JSONArray array = new JSONArray(list);
-		//2. 텍스트 파일로 출력
-		try(FileWriter fw = new FileWriter("student.json");){
+		// 2. 텍스트 파일로 출력
+		try (FileWriter fw = new FileWriter("student.json");) {
 			fw.write(array.toString());
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	//student.json 파일을 텍스트로 전부 읽어옴
-	//텍스트를 JSON으로 변환
-	//JSONObject를 하나씩 읽어서 ArrayList에 추가
+
+	// student.json 파일을 텍스트로 전부 읽어옴
+	// 텍스트를 JSON으로 변환
+	// JSONObject를 하나씩 읽어서 ArrayList에 추가
 	public void loadStudentJSONFile() {
-		
+		// 1. 파일 읽기
+		try (FileReader fr = new FileReader("student.json"); 
+				BufferedReader br = new BufferedReader(fr);) {
+			String str = "", s;
+			while((s = br.readLine()) != null) {
+				str += s;
+			}
+			// 2. JSONArray로 변경
+			JSONArray jsonArray = new JSONArray(str);
+			// 3. JSONObject로 한건 씩 읽어서 StudentVO 생성해서 list에 저장
+			for(int i=0;i<jsonArray.length();i++) {
+				JSONObject json = jsonArray.getJSONObject(i);
+				list.add(new StudentVO(json.getString("studentNo"), 
+						json.getString("studentName"),
+						json.getString("major"),
+						json.getDouble("score")));
+			}
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
