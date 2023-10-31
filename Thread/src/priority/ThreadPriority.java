2882afbc543cd9a9eb4f0953cc4cdfeb2ec47014
nam2626 <nam2626@gmail.com>
2023-10-31 12:44:20 +0900

    Thread

---
+++ Thread/src/priority/ThreadPriority.java
@@ -0,0 +1,19 @@
+package priority;
+
+public class ThreadPriority extends Thread {
+
+	public ThreadPriority(String name) {
+		super(name);
+	}
+	
+	@Override
+	public void run() {
+		try {
+			for (int i = 0; i < 100; i++)
+				Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(getName() + " - 스레드 종료 -" +getPriority());
+	}
+}
