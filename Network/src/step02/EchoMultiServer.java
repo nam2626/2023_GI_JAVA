d159476cd75453b4452d13b242044dc76e5f47e3
nam2626 <nam2626@gmail.com>
2023-11-01 15:12:08 +0900

    Socket Programming

--- Network/src/step02/EchoMultiServer.java
+++ Network/src/step02/EchoMultiServer.java
@@ -1,9 +1,30 @@
 package step02;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class EchoMultiServer {
 
 	public static void main(String[] args) {
-		
+		try (ServerSocket server = new ServerSocket(1234);) {
+			System.out.println("에코 서버 오픈 완료....");
+
+			Socket client = server.accept();
+			System.out.println(client.getInetAddress() + "님이 접속하셨습니다.");
+			EchoServerWorker worker = new EchoServerWorker(client);
+			worker.start();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
+
+
+
+
+
+
+
