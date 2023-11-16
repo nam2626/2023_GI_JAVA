2f653b6be10213ebb2a5bb2a7931ecc489842cff
nam2626 <nam2626@gmail.com>
2023-11-16 15:12:16 +0900

    jdbc

--- StudentProject_jdbc/src/controller/AppendStudentController.java
+++ StudentProject_jdbc/src/controller/AppendStudentController.java
@@ -16,17 +16,17 @@ public class AppendStudentController implements Controller {
 		System.out.print("이름 : ");
 		String studentName = sc.nextLine(); 
 		//학과번호 입력 받는 것으로 변경
-		System.out.print("학과 : ");
-		String major = sc.nextLine();
+		System.out.print("학과 번호: ");
+		int majorNo = sc.nextInt(); sc.nextLine();
 		System.out.print("평점 : ");
 		double score = sc.nextDouble();
 		sc.nextLine();
 		//성별 입력
-		
-		//StudentVO 클래스를 변경
-		//Service클래스로 등록할 정보를 보내고, 결과를 받음
+		System.out.print("성별 : ");
+		String gender = sc.nextLine();
+
 		boolean result = StudentService.getInstance().insertStudent(
-				new StudentVO(studentNo, studentName, major, score));
+				new StudentVO(studentNo, studentName, majorNo, score, gender));
 		
 		if(result)
 			System.out.println("학생 정보 등록 성공");
